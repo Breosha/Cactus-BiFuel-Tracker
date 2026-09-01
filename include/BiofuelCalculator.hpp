aebccddaa09e75147da1edbfe201c14bb8deb0e9
Breosha <bbreosha@icloud.com>
2026-09-01 15:27:29 -0400

    Implement BiofuelCalculator for yield calculations
    Added BiofuelCalculator class with yield calculation method.

--- include/BiofuelCalculator.hpp
+++ include/BiofuelCalculator.hpp
@@ -1,1 +1,18 @@
+#pragma once
+#include "CactusSpecies.hpp"
 
+struct SimulationResult {
+    double totalFreshMassKg;
+    double dryMassKg;
+    double waterMassKg;
+    double estimatedYieldLiters;
+    double totalEnergyOutputMJ;
+};
+
+class BiofuelCalculator {
+public:
+    static SimulationResult calculateYield(const CactusSpecies& species, 
+                                           double freshMassKg, 
+                                           double moisturePercent, 
+                                           double conversionRate);
+};
